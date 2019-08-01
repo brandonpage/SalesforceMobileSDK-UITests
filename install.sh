e3f5443db504d87ba4fd6bd8bf9142eb3cde3365
brandonpage <bpage1@salesforce.com>
2019-07-31 19:30:33 -0700

    Fix reporting

--- install.sh
+++ install.sh
@@ -10,8 +10,10 @@ else
     npm install -g cordova@8.1.2
     cordova telemetry off
 
-    echo y | sudo gem uninstall cocoapods
-    sudo gem install cocoapods -v 1.6.0
+    if [ $(pod --version) != "1.6.0" ]; then
+        echo y | sudo gem uninstall cocoapods
+        sudo gem install cocoapods -v 1.6.0
+    fi
 fi
 
 git clone --branch dev --single-branch --depth 1 https://github.com/forcedotcom/SalesforceMobileSDK-Package.git
