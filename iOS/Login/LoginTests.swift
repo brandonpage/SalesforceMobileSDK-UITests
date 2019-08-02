e883c4dc2d9bb7e0c1f2ce474758c9b23c6cc07d
brandonpage <bpage1@salesforce.com>
2019-08-02 10:03:32 -0700

    Test RN in CI

--- iOS/Login/LoginTests.swift
+++ iOS/Login/LoginTests.swift
@@ -76,7 +76,6 @@ class LoginTests: XCTestCase {
         case .reactNative:
             print(app.otherElements)
             let titleElement = app.otherElements.matching(identifier: sampleAppTitle).staticTexts[sampleAppTitle]
-            //let titleElement = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other)[reactNativeUsers].children(matching: .other)[reactNativeUsers].children(matching: .other)[reactNativeUsers].children(matching: .other)[reactNativeUsers].children(matching: .other)[reactNativeUsers].children(matching: .other)[sampleAppTitle].children(matching: .other)[sampleAppTitle].children(matching: .other)[sampleAppTitle]
             XCTAssert(titleElement.waitForExistence(timeout: timeout), appLoadError)
         }
     }
